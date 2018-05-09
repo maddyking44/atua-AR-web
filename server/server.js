61e6243d12f3baae0cbc1831a95c663008c72bd4
zachmandeville <zach@apileof.rocks>
2018-05-09 20:53:46 +1200

    testing configuration of author again

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//This file fires up our server, connects it to our front-end, and directs url requests to their proper route functions.
+//This fires up our server, connects it to our front-end, and directs url requests to their proper route functions.
 
 
 const path = require('path')
