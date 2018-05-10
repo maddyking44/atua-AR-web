5285dce18eba0ff03812be32e2f289891badf253
maddyking44 <madelineking44@gmail.com>
2018-05-10 14:40:19 +1200

    weather api connected and displaying properly

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -6,7 +6,10 @@ class Home extends React.Component {
   render() {
     return (
     <div>
-      {this.props.weather.wind_dir}
+      <div>Current Weather status: {this.props.weather.weather}</div>
+      <div>Wind Direction: {this.props.weather.wind_dir}</div>
+      <div>Wind Speed in kph: {this.props.weather.wind_kph}</div>
+      <img src={this.props.weather.icon_url}/>
     </div>
     )
   }
