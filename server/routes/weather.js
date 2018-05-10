0ce8a4b7d2e20b84a0c71b10293509463997d340
zachmandeville <zach@apileof.rocks>
2018-05-10 14:44:40 +1200

    fixed spelling error

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -8,7 +8,7 @@ router.use(express.json())
 router.get('/', (req, res)=> {
     request
       .get(`http://api.wunderground.com/api/38b4b1221db21c51/conditions/q/New_Zealand/Wellington.json`)
-      .end((err, result)=> {iff(err){
+      .end((err, result)=> {if(err){
           res.status(500).send(err.message)
           } else {
             res.json(result.body.data.children)
