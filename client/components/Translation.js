ff8a07200f74be17df1948e43511a020c55219ff
zachmandeville <zach@apileof.rocks>
2018-05-10 14:40:34 +1200

    Sentence changes upon hover
    I added the first bit of functionality so that each sentence in a body
    of text changes upon the hover.

--- client/components/Translation.js
+++ client/components/Translation.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import { connect as buildWifiContainer } from 'react-redux'
+import { connect } from 'react-redux'
 
 var Translation = ({posts}) => (
   <div>
-  <h2>Congratulations, you have made it to the Translations Component!</h2>
-  <ul>
+    <h2>Congratulations, you have made it to the Translations Component!</h2>
+    <p className='paragraph'>
+      <span className='sentence'>
+	<span className='te-reo'>kia ora </span>
+	<span className='english'>hello </span>
+      </span>
+      <span className='sentence'>
+      <span className='te-reo'>Mā te whakaaro nui e hanga te whare; mā te mātauranga e whakaū</span>
+      <span className='english'>Big ideas create the house; knowledge maintains it.</span>
+      </span>
+    </p>
+  </div>
+
+  /*  <ul>
   {posts.map(({id, title}) => (
     <li key={id}>{title}</li>
   )
   )}
-  </ul>
-  </div>
+  </ul> */
 )
 
 const mapStateToProps = ({posts}) => ({posts})
 
-const wifiContainer = buildWifiContainer(mapStateToProps)
-const wifiComponent = wifiContainer(Translation)
-
-export default wifiComponent
+export default connect(mapStateToProps)(Translation)
