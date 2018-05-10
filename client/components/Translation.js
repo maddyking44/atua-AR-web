f3a2000b481797fbbf0d071192afa81cd6da053c
zachmandeville <zach@apileof.rocks>
2018-05-10 20:03:21 +1200

    Add animation for translation on hover
    I added a basic animation to show what it would look like to hover over
    a sentence and have it translate, and not have that experience be mad
    unpleasant.  Now there's some blurry text that settles into the
    translated text.  I am not sure if it is mad unpleasant, but it is at least a
    little softer than before.

--- client/components/Translation.js
+++ client/components/Translation.js
@@ -2,28 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 var Translation = ({posts}) => (
-  <div>
-    <h2>Congratulations, you have made it to the Translations Component!</h2>
+  <section id='article'>
+    <h2><span className='sentence'><span className='te-reo'>Kia Ora</span><span className='english'>Hello!</span></span></h2>
     <p className='paragraph'>
       <span className='sentence'>
-	<span className='te-reo'>kia ora </span>
-	<span className='english'>hello </span>
+	<span className='te-reo'>kia ora!</span>
+	<span className='english'>hello!</span>
       </span>
       <span className='sentence'>
       <span className='te-reo'>Mā te whakaaro nui e hanga te whare; mā te mātauranga e whakaū</span>
       <span className='english'>Big ideas create the house; knowledge maintains it.</span>
       </span>
     </p>
-  </div>
-
-  /*  <ul>
-  {posts.map(({id, title}) => (
-    <li key={id}>{title}</li>
-  )
-  )}
-  </ul> */
+  </section>
 )
 
 const mapStateToProps = ({posts}) => ({posts})
 
 export default connect(mapStateToProps)(Translation)
+
+
