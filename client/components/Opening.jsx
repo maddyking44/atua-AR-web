886442666a97d94ddfeee68bc628f121d55abecb
l-suzuki <lehsuzuki@gmail.com>
2018-05-15 15:38:02 +1200

    text appearing in pairs now

--- client/components/Opening.jsx
+++ client/components/Opening.jsx
@@ -31,11 +31,11 @@ revealAll() {
 render(){
   return (
     <div className='opening'>
-      {this.state.isVisible.find(text => text=='ancient') && <div className='ancient fade-in' onClick={()=>this.makeVisible('contemporary')}>culture</div>}
-      {this.state.isVisible.find(text => text =='contemporary') && <div className='spiritual fade-in' onClick={()=>this.makeVisible('spiritual')}>technology</div>}
-      {this.state.isVisible.find(text => text== 'spiritual') && <div className='contemporary fade-in'onClick={()=>this.makeVisible('physical')}>past</div>}
-      {this.state.isVisible.find(text => text == 'physical') && <div className='physical fade-in' onClick={()=>this.makeVisible('wairoa')}>future</div>}
-      {this.state.isVisible.find(text => text == 'wairoa') && <div className='physical fade-in' onClick={()=>this.makeVisible('marama')}>Te Ao Wairoa</div>}
+      {this.state.isVisible.find(text => text=='ancient' || text=='technology') && <div className='ancient fade-in' onClick={()=>this.makeVisible('technology')}>culture</div>}
+      {this.state.isVisible.find(text => text =='technology') && <div className='spiritual fade-in' onClick={()=>this.makeVisible('past')}>technology</div>}
+      {this.state.isVisible.find(text => text== 'past' || text == 'future') && <div className='contemporary fade-in'onClick={()=>this.makeVisible('future')}>past</div>}
+      {this.state.isVisible.find(text => text == 'future') && <div className='physical fade-in' onClick={()=>this.makeVisible('wairoa')}>future</div>}
+      {this.state.isVisible.find(text => text == 'wairoa' ||text == 'marama') && <div className='physical fade-in' onClick={()=>this.makeVisible('marama')}>Te Ao Wairoa</div>}
       {this.state.isVisible.find(text => text == 'marama') && <div className='physical fade-in' onClick={this.revealAll}>Te Ao Marama</div>}
         {this.state.isVisible.find(text => text == 'enter') && <a className='enter-site fade-in' onClick={()=>this.props.dispatch(navigate('home'))}>enter</a>}
     {this.state.unified && <div className='together fade-in' onClick={()=>this.makeVisible('enter')}>
