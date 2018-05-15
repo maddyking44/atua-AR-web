aee64e5c94aca23a7e886c13f04089b90e61f28e
l-suzuki <lehsuzuki@gmail.com>
2018-05-15 16:32:30 +1200

    positioned the divs properly and added classes

--- client/components/Opening.jsx
+++ client/components/Opening.jsx
@@ -16,7 +16,8 @@ class Opening extends React.Component {
 
 makeVisible(text) {
     this.setState({
-      isVisible: [text]
+      isVisible: [text],
+      unified: false
     })
 }
 
@@ -31,20 +32,20 @@ revealAll() {
 render(){
   return (
     <div className='opening'>
-      {this.state.isVisible.find(text => text=='ancient' || text=='technology') && <div className='ancient fade-in' onClick={()=>this.makeVisible('technology')}>culture</div>}
-      {this.state.isVisible.find(text => text =='technology') && <div className='spiritual fade-in' onClick={()=>this.makeVisible('past')}>technology</div>}
-      {this.state.isVisible.find(text => text== 'past' || text == 'future') && <div className='contemporary fade-in'onClick={()=>this.makeVisible('future')}>past</div>}
-      {this.state.isVisible.find(text => text == 'future') && <div className='physical fade-in' onClick={()=>this.makeVisible('wairoa')}>future</div>}
-      {this.state.isVisible.find(text => text == 'wairoa' ||text == 'marama') && <div className='physical fade-in' onClick={()=>this.makeVisible('marama')}>Te Ao Wairoa</div>}
-      {this.state.isVisible.find(text => text == 'marama') && <div className='physical fade-in' onClick={this.revealAll}>Te Ao Marama</div>}
+      {this.state.isVisible.find(text => text=='ancient' || text=='technology') && <div className='left-things fade-in' onClick={()=>this.makeVisible('technology')}>culture</div>}
+      {this.state.isVisible.find(text => text =='technology') && <div className='right-things fade-in' onClick={()=>this.makeVisible('past')}>technology</div>}
+      {this.state.isVisible.find(text => text== 'past' || text == 'future') && <div className='left-things fade-in'onClick={()=>this.makeVisible('future')}>past</div>}
+      {this.state.isVisible.find(text => text == 'future') && <div className='right-things fade-in' onClick={()=>this.makeVisible('wairoa')}>future</div>}
+      {this.state.isVisible.find(text => text == 'wairoa' ||text == 'marama') && <div className='left-things fade-in' onClick={()=>this.makeVisible('marama')}>Te Ao Wairoa</div>}
+      {this.state.isVisible.find(text => text == 'marama') && <div className='right-things fade-in' onClick={this.revealAll}>Te Ao Marama</div>}
         {this.state.isVisible.find(text => text == 'enter') && <a className='enter-site fade-in' onClick={()=>this.props.dispatch(navigate('home'))}>enter</a>}
     {this.state.unified && <div className='together fade-in' onClick={()=>this.makeVisible('enter')}>
-      <div>culture</div>
-      <div>technology</div>
-      <div>past</div>
-      <div>future</div>
-      <div>Te Ao Wairoa</div>
-      <div>Te Ao Marama</div>
+      <div className='tog-thin'>culture</div>
+      <div className='tog-fat'>technology</div>
+      <div className='tog-thin'>past</div>
+      <div className='tog-fat'>future</div>
+      <div className='tog-thin'>Te Ao Wairoa</div>
+      <div className='tog-fat'>Te Ao Marama</div>
     </div>}
     </div>
 
