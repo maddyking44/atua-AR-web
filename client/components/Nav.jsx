b8f6b74747d619037e6db7a219fc384fc4737eee
zachmandeville <zach@apileof.rocks>
2018-05-16 16:52:17 +1200

    Move Learn More to Article List
    We had a learn more transition happening at the bottom window, that was
    bad UX and unnecessarily hard.  I removed that element from the window
    and then took the text and added it to our Db as a new article.

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -6,29 +6,20 @@ import {getTextByURL} from '../actions/text'
 const Nav = (props)=> {
   return (
     <div>
-      <a href="javascript:void(0);" className='close' onClick={props.closeNav}>&times;</a>
-    {props.navVisible != true &&
-      <div className='explanation'>
-        <p className='text'>
-          <span className='english'>This visualisation is inspired by the legend of Tawhirimatea. Its form changes depending on the wind speed and direction in Te Whanganui-a-Tara Wellington.</span>
-          <span className='te-reo'>TE REO NEEDED FOR EXPLANATION</span>
-        </p>
-        <a href="javascript:void(0);" className='learn-more fade-in' onClick={()=>props.dispatch(flipNav(true))}>Learn More</a>
-      </div> }
-    {props.navVisible && <ul id='navItems'>
-       {props.texts.map(text => {
+    <a href="javascript:void(0);" className='close' onClick={props.closeNav}>&times;</a>
+    <ul id='navItems'>
+    {props.texts.map(text => {
       return (
-	  <li key={text.id}>
-	       <a className='text' title={`link to ${text.url}`} href='#' onClick={() => {props.dispatch(getTextByURL(text.url))}}>
-		 <span className='te-reo'>{text.short_title.tereo}</span>
-		 <span className='english'>{text.short_title.english}</span>
-	       </a>
-	     </li>
+	<li key={text.id}>
+	  <a className='text' title={`link to ${text.url}`} href='#' onClick={() => {props.dispatch(getTextByURL(text.url))}}>
+	    <span className='te-reo'>{text.short_title.tereo}</span>
+	    <span className='english'>{text.short_title.english}</span>
+	  </a>
+	</li>
       )
     })
     }
     </ul>
-  }
     </div>
   )
 }
@@ -41,3 +32,4 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Nav)
+
