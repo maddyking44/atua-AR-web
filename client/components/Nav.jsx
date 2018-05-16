f74225950dff97c4baa65e91e96e28ba257c58ad
l-suzuki <lehsuzuki@gmail.com>
2018-05-16 12:52:22 +1200

    styled the nav bar and wind info box

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {flipNav} from '../actions/nav'
+import {flipNav, closeNav} from '../actions/nav'
 import {getTextByURL} from '../actions/text'
 
 const Nav = (props)=> {
   return (
     <div>
-    <a href="javascript:void(0);" className='close' onClick={props.closeNav}>&times;</a>
-    {props.navVisible != true && <a href="javascript:void(0);" className='learn-more fade-in' onClick={()=>props.dispatch(flipNav(true))}>Learn More</a>}
+      <a href="javascript:void(0);" className='close' onClick={props.closeNav}>&times;</a>
+    {props.navVisible != true &&
+      <div className='explanation'>
+        <p className='text'>
+          <span className='english'>This visualisation is inspired by the legend of Tawhirimatea. Its form changes depending on the wind speed and direction in Te Whanganui-a-Tara Wellington.</span>
+          <span className='te-reo'>TE REO NEEDED FOR EXPLANATION</span>
+        </p>
+        <a href="javascript:void(0);" className='learn-more fade-in' onClick={()=>props.dispatch(flipNav(true))}>Learn More</a>
+      </div> }
     {props.navVisible && <ul id='navItems'>
        {props.texts.map(text => {
       return (
