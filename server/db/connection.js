370a751eb08b9cfc5990e7c2372d2ec503112131
maddyking44 <madelineking44@gmail.com>
2018-05-27 21:49:55 +1200

    videos looping

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
-const environment = process.env.NODE_ENV || 'development'
-const config = require('../../knexfile')[environment]
-const connection = require('knex')(config)
+var environment = process.env.NODE_ENV || 'development'
+var config = require('../../knexfile')[environment]
+var connection = require('knex')(config)
 
 //require this module into db files to use as the db connection
   //const db = require('./connection')
